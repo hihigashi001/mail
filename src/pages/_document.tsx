9c0268ad0c1099bae9217fb28cceef60b27a192b
hihigashi001 <hihigashi001@gmail.com>
2021-06-14 09:20:09 +0900

    favicon error修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="favi16.png" sizes="16x16" type="image/png" />
+          <link rel="icon" href="favi32.png" sizes="32x32" type="image/png" />
+          <link rel="icon" href="favi48.png" sizes="48x48" type="image/png" />
+          <link rel="icon" href="favi62.png" sizes="62x62" type="image/png" />
+          <link rel="apple-touch-icon-precomposed" href="favi150.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
